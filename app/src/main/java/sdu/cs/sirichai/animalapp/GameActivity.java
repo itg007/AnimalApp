bab553bb88172fd273bebade44f62ad9be3f3e13
sirichai <lloll171@hotmail.com>
2019-02-28 11:33:01 +0700

    Animal App 28/2/62 ผูกตัวแปร

--- app/src/main/java/sdu/cs/sirichai/animalapp/GameActivity.java
+++ app/src/main/java/sdu/cs/sirichai/animalapp/GameActivity.java
@@ -2,12 +2,29 @@ package sdu.cs.sirichai.animalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
+
+    //Explicit
+    Button btn1,btn2,btn3,btn4;
+    ImageView questionImageView;
+    ImageButton volumeImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-    }
-}
+
+        //ผูกตัวแปรบน java กับ id บน xml
+        btn1 = findViewById(R.id.btm1);
+        btn2 = findViewById(R.id.btm2);
+        btn3 = findViewById(R.id.btm3);
+        btn4 = findViewById(R.id.btm4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumeImageButton = findViewById(R.id.imbVolume);
+
+    }//end onCreate
+}//end Class
